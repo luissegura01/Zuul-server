3d4e276a275030dcfbfbdbfe66b77a9259e2a15d
luissegura01 <39246877+luissegura01@users.noreply.github.com>
2019-02-08 12:59:08 -0500

    Rest template added

--- Microservice1/src/main/java/com/luis/microservice1/Microservice1Application.java
+++ Microservice1/src/main/java/com/luis/microservice1/Microservice1Application.java
@@ -3,6 +3,8 @@ package com.luis.microservice1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,7 +12,6 @@ public class Microservice1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Microservice1Application.class, args);
-
 	}
 
 }
--- Microservice1/src/main/java/com/luis/microservice1/controller/Microservice1Controller.java
+++ Microservice1/src/main/java/com/luis/microservice1/controller/Microservice1Controller.java
@@ -1,14 +1,29 @@
 package com.luis.microservice1.controller;
 
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
+@EnableHystrix
 public class Microservice1Controller {
 	
 	@RequestMapping(value = "/teachers")
-	public String greeting() {
-		return "Hello from microservice1";
+	@HystrixCommand(fallbackMethod = "fallback_hello")
+	public String greeting() throws InterruptedException {
+		//Thread.sleep(3000);
+		//return "Hello from microservice1";
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject("http://localhost:8082/students", 
+		          String.class);
+		return "microservice1 calling microservice2, " + result;
 	}
+	
+	private String fallback_hello() {
+		   return "Request fails. Please try again later";
+		}
 
 }
